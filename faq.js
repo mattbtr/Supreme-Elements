06aa29868a1b2a20d7075297ed018de7d4a300c7
mattbtr <m.beiterhhh@googlemail.com>
2023-11-10 10:37:28 +0100

    formatted

--- faq.js
+++ faq.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    initApp();
-  });
-  
-  function initApp() {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-  
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      });
-    }
+  initApp();
+});
+
+function initApp() {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
   }
-  
+}
