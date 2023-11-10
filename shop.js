79480a0bff1c3ab0585f279b8496ae3d69b10349
Johannes <johannes@no-reply.com>
2023-11-10 14:41:42 +0100

    einheitlich Eirücken

--- shop.js
+++ shop.js
@@ -1,9 +1,9 @@
 function Danksagung() {
-    alert("Vielen Dank für Ihren Einkauf bei Supreme Elements.")
+  alert("Vielen Dank für Ihren Einkauf bei Supreme Elements.")
 }
 
 function WeiterleitungDetails() {
-    window.location.href = 'produktdetails.html';
+  window.location.href = 'produktdetails.html';
 }
 
 
@@ -11,19 +11,19 @@ function WeiterleitungWarenkorb() {
   window.location.href = 'warenkorb.html';
 }
 
-function mehrProdukteJS() {    
-    document.getElementById("Produktliste2").style.display = "table"; 
-    document.getElementById("zeigeMehr").style.display = "none";
-    document.getElementById("wenigerZeigen").style.display = "block";
-    document.getElementById("blickpunktMehr").scrollIntoView({ behavior: "smooth" });
-  }
-  
-  function wenigerZeigen() {
-    document.getElementById("Produktliste2").style.display = "none"; 
-    document.getElementById("zeigeMehr").style.display = "block";
-    document.getElementById("wenigerZeigen").style.display = "none";
-    document.getElementById("blickpunktWeniger").scrollIntoView({ behavior: "smooth" });
-  }
+function mehrProdukteJS() {
+  document.getElementById("Produktliste2").style.display = "table";
+  document.getElementById("zeigeMehr").style.display = "none";
+  document.getElementById("wenigerZeigen").style.display = "block";
+  document.getElementById("blickpunktMehr").scrollIntoView({ behavior: "smooth" });
+}
 
-  
-  
+function wenigerZeigen() {
+  document.getElementById("Produktliste2").style.display = "none";
+  document.getElementById("zeigeMehr").style.display = "block";
+  document.getElementById("wenigerZeigen").style.display = "none";
+  document.getElementById("blickpunktWeniger").scrollIntoView({ behavior: "smooth" });
+}
+
+
+
