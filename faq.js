1d4c151fa70eb64f01248f1f67211cdd6607041d
mattbtr <m.beiterhhh@googlemail.com>
2023-12-01 14:01:25 +0100

    dynamisches kontaktformular grundstein

--- faq.js
+++ faq.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  initApp();
+  faq();
+  submitForm();
 });
 
-function initApp() {
+function faq() {
   var acc = document.getElementsByClassName("accordion");
   var i;
 
@@ -18,3 +19,53 @@ function initApp() {
     });
   }
 }
+
+function submitForm(){
+  const formKontakt = document.querySelector('#kontaktform')
+
+  formKontakt.addEventListener('submit', event =>{
+    event.preventDefault();
+
+    const formData = new FormData(formKontakt);
+    const data = Object.fromEntries(formData);
+  
+    fetch('http://localhost:8000/api/kontaktaufnahme', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  })
+}
+
+/*
+$('#kontaktform').submit(function (e) { 
+  e.preventDefault();
+
+  let formData = new FormData(this);
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8000/api/kontaktaufnahme",
+    data: formData,
+    contentType: false,
+    cache: false,
+    processData: false,
+    dataType: "json",
+  });
+  .done(function(response){
+    console.log('response erhalten')
+    console.log(response)
+    $(#form-uebergeben).html('<p>Operation erfolgereich.</p>')
+    $(#form-uebergeben).append('<p>Nachricht: ' + JSON.stringify(response) + '</p>')
+  })
+  .fail(function(xhr) {
+    console.log('error received');
+    console.log(xhr);
+    $('#output').html('<p>Es ist ein Fehler aufgetreten</p>');
+    $('#output').append('<p>Status: ' + xhr.status + '</p>');
+    $('#output').append('<p>Nachricht: ' + xhr.responseText + '</p>');
+})
+  
+});*/
