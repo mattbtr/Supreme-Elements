cc788add95f0bd13c24448a54327bc9304b8e0d6
mattbtr <m.beiterhhh@googlemail.com>
2023-12-12 17:31:34 +0100

    kontaktformular dynamisch

--- frontend/faq.js
+++ frontend/faq.js
@@ -1,3 +1,4 @@
+//<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>;
 document.addEventListener("DOMContentLoaded", () => {
   faq();
   submitForm();
@@ -19,53 +20,116 @@ function faq() {
     });
   }
 }
+/*
+function submitForm(){
+  const kontaktform = document.getElementById("kontaktform");
+  console.log("form submit called");
+  kontaktform.addEventListener("submit", event => {
+      event.preventDefault();
+
+      const formData = new FormData(kontaktform);
+      console.log(formData.get('vornameInput'));
+      console.log(formData.get("nachnameInput"));
+      console.log(formData.get("emailInput"));
+      console.log(formData.get("nachrichtInput"));
+      const data = Object.fromEntries(formData);
+
+      fetch("http://localhost:8000/api/kontaktaufnahme", {
+        method: "POST",
+        body: formData,
+        headers: {
+          'Content-Type': "application/json"
+        },
+          body: JSON.stringify(data)
+      });
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("response erhalten");
+          console.log(data);
+          document.getElementById("form-uebergeben").innerHTML =
+            "<p>Operation erfolgreich.</p>";
+          document.getElementById("form-uebergeben").innerHTML +=
+            "<p>Nachricht: " + JSON.stringify(data) + "</p>";
+        })
+        .catch((error) => {
+          console.error("error received");
+          console.error(error);
+          document.getElementById("form-uebergeben").innerHTML =
+            "<p>Es ist ein Fehler aufgetreten</p>";
+          document.getElementById("form-uebergeben").innerHTML +=
+            "<p>Status: " + error.status + "</p>";
+          document.getElementById("form-uebergeben").innerHTML +=
+            "<p>Nachricht: " + error.message + "</p>";
+        });
+    });
+  }*/
+  
+  
+
+
 
 function submitForm(){
-  const formKontakt = document.querySelector('#kontaktform')
+  $("#kontaktform").submit(function (event) {
+    console.log("form submit called");
 
-  formKontakt.addEventListener('submit', event =>{
+    // disable default event
     event.preventDefault();
 
-    const formData = new FormData(formKontakt);
-    const data = Object.fromEntries(formData);
-  
-    fetch('http://localhost:8000/api/kontaktaufnahme', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-  })
+    // convert data of form to object
+    var formData = {
+      vorname: $("#vornameInput").val(),
+      nachname: $("#nachnameInput").val(),
+      email: $("#emailInput").val(),
+      nachricht: $("#nachrichtInput").val(),
+    };
+
+    // Send form data with AJAX
+    $.ajax({
+      url: "http://localhost:8000/api/kontaktaufnahme",
+      type: "POST",
+      data: JSON.stringify(formData),
+      contentType: "application/json",
+      dataType: "json",
+    });
+
+    
+    /*
+      .done(function (response) {
+        console.log(formData)
+        console.log("response received");
+        console.log(response);
+        $("#form-uebergeben").html("<p>Operation erfolgreich</p>");
+        $("#form-uebergeben").append(
+          "<p>Anzahl Form-Elemente: " + response.length + "</p>"
+        );
+        response.forEach((element) => {
+          $("#form-uebergeben").append(
+            element.vo
+          );
+        });
+      })
+      .fail(function (xhr) {
+        console.log("error received");
+        console.log(xhr);
+        $("#form-uebergeben").html("<p>Es ist ein Fehler aufgetreten</p>");
+        $("#form-uebergeben").append("<p>Status: " + xhr.status + "</p>");
+        $("#form-uebergeben").append(
+          "<p>Nachricht: " + xhr.responseText + "</p>"
+        );
+      });*/
+  });
+
+
 }
 
-/*
-$('#kontaktform').submit(function (e) { 
-  e.preventDefault();
 
-  let formData = new FormData(this);
 
-  $.ajax({
-    type: "POST",
-    url: "http://localhost:8000/api/kontaktaufnahme",
-    data: formData,
-    contentType: false,
-    cache: false,
-    processData: false,
-    dataType: "json",
-  });
-  .done(function(response){
-    console.log('response erhalten')
-    console.log(response)
-    $(#form-uebergeben).html('<p>Operation erfolgereich.</p>')
-    $(#form-uebergeben).append('<p>Nachricht: ' + JSON.stringify(response) + '</p>')
-  })
-  .fail(function(xhr) {
-    console.log('error received');
-    console.log(xhr);
-    $('#output').html('<p>Es ist ein Fehler aufgetreten</p>');
-    $('#output').append('<p>Status: ' + xhr.status + '</p>');
-    $('#output').append('<p>Nachricht: ' + xhr.responseText + '</p>');
-})
-  
-});*/
+// Der post von ajax funktioniert nicht. Daten von form werden nicht richtig in formData gespeichert. Irwas an post ajax befehl passt nicht
+// Lösung WAR: Dao hat nciht gepasst. Wollte Id als Parameter noch empfangen.
+// Das rausnehmen!! Dann wird Id autoincrement automatisch zu Daten angelegt!
+
