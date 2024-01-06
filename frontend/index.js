6e1983c5f6386d16423f787f274d0ab6cce58726
mattbtr <m.beiterhhh@googlemail.com>
2024-01-06 12:54:44 +0100

    alert

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
   submitNewsletterUeberUns();
   submitNewsletterIndex();
   loadSliderProducts();
 
-  window.setTimeout( function() {
+  window.setTimeout(function () {
     // CODE VERZÖGERT
     $('.slider').slick({ //slider Funktionalität laden
       autoplay: true,
@@ -11,40 +11,40 @@ $(document).ready(function(){
       arrows: true,
       dots: true,
     });
-   }, 500);
+  }, 500);
 
-   
-   $(document).on('click', '.addToCart', function() {     // funktioniert auch, wenn Elemente nachgeladen werden (slider) -> dynamsich
-   
+
+  $(document).on('click', '.addToCart', function () {     // funktioniert auch, wenn Elemente nachgeladen werden (slider) -> dynamsich
+
     var selectedProductid = $(this).data('productid');    // holt html Attribut: data-productid aus slider
-  
+
     console.log("Produkt in den Warenkorb: " + selectedProductid);
     addToBasket(selectedProductid);
-  });  
+  });
 });
 
 function loadSliderProducts() {
   $.ajax({
-      url: 'http://localhost:8000/api/produkt/isHighlight',
-      method: 'get',
-      contentType: 'application/json; charset=utf-8',
-      cache: false,
-      dataType: 'json'
+    url: 'http://localhost:8000/api/produkt/isHighlight',
+    method: 'get',
+    contentType: 'application/json; charset=utf-8',
+    cache: false,
+    dataType: 'json'
   }).done(function (response) { //wird ausgeführt, wenn geladen
-      console.log(response);
-      $(response).each(function(idx, item) {
-        
-        var sliderHTML = '<div class="sliderElement"><br /><a href="produktdetails.html"><img class="shopImg" src="' + item.produktbild + '"/></a><div class="infoBox"><br /><b>' + item.bezeichnung + '</b><br />' + item.bruttopreis + ' €<br /><button class="addToCart" data-productid="' + item.id + '" >In den Warenkorb</button></div></div>';
-        $('#sliderContainer').append(sliderHTML);
+    console.log(response);
+    $(response).each(function (idx, item) {
 
-      });    
+      var sliderHTML = '<div class="sliderElement"><br /><a href="produktdetails.html"><img class="shopImg" src="' + item.produktbild + '"/></a><div class="infoBox"><br /><b>' + item.bezeichnung + '</b><br />' + item.bruttopreis + ' €<br /><button class="addToCart" data-productid="' + item.id + '" >In den Warenkorb</button></div></div>';
+      $('#sliderContainer').append(sliderHTML);
+
+    });
   }).fail(function (jqXHR, statusText, error) {
-      console.log('Response Code: ' + jqXHR.status + ' - Fehlermeldung: ' + jqXHR.responseText);
-  }); 
+    console.log('Response Code: ' + jqXHR.status + ' - Fehlermeldung: ' + jqXHR.responseText);
+  });
 };
 
 
-function submitNewsletterIndex(){
+function submitNewsletterIndex() {
   $("#newsletterFormIndex").submit(function (event) {
     console.log("form submit called");
 
@@ -63,7 +63,14 @@ function submitNewsletterIndex(){
       data: JSON.stringify(formData),
       contentType: "application/json",
       dataType: "json",
-    });
+    }).done(function (response) {
+      console.log(response);
+      alert("Sie sind ab sofort bei unserem Newsletter angemeldet.");
+
+    })
+      .fail(function () {
+        alert('Ein Fehler ist aufgetreten');
+      });
   });
 }
 
@@ -86,6 +93,15 @@ function submitNewsletterUeberUns() {
       data: JSON.stringify(formData),
       contentType: "application/json",
       dataType: "json",
-    });
+    }).done(function (response) {
+      console.log(response);
+      alert("Sie sind ab sofort bei unserem Newsletter angemeldet.");
+
+    })
+      .fail(function () {
+        console.log(xhr.responseText);  // Zeigt die Serverantwort im Konsolenprotokoll an
+        alert('Ein Fehler ist aufgetreten');
+
+      })
   });
-}
+};
