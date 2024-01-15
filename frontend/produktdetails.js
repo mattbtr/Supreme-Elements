136dfb652fb1341b36ddfef2432aab395696c513
AbdulhamitGd <abdulhamit.gd@web.de>
2024-01-15 14:40:46 +0100

    kleiner Anpassungen Produktdetailseite

--- frontend/produktdetails.js
+++ frontend/produktdetails.js
@@ -1,34 +1,26 @@
-function Meldung() {
+/*function Meldung() {
     alert("Das Produkt wurde dem Warenkorb hinzugefügt!")
-}
+}*/
 
 function ZurückProdukte() {
   window.location.href = 'shop.html';
 }
 $(document).ready(function() {
   console.log('loading specific product');
-
-  // get id from url parameter
-   var id = -1;
-
-  // Debugging: Print the entire URL
+  var id = -1;
   console.log('Current URL: ' + window.location.href);
-
   if (existsUrlParameter('id')) {
       id = getUrlParameterValue('id');
   }
-
-  // Debugging: Print the extracted ID
-  console.log('Extracted ID: ' + id);
-
   console.log('target product id=' + id);
 
   if (id == -1) {
       alert('keine id erhalten');
       return;
   }
+  console.log('Current URL: ' + window.location.href);
 
-  /*if (existsUrlParameter('id')) 
+  if (existsUrlParameter('id')) 
       id = getUrlParameterValue('id');
 
   console.log('target product id=' + id);
@@ -36,7 +28,17 @@ $(document).ready(function() {
   if (id == -1) {
       alert('keine id erhalten');
       return;
-  } */
+  } 
+
+  $(document).on('click', '.addToCart', function () {     
+    var selectedProductid = getUrlParameterValue('id');
+    if (selectedProductid === null) {
+      console.log('Invalid or missing id');
+      return;
+    }
+    console.log("Produkt in den Warenkorb: " + selectedProductid);
+    addToBasket(selectedProductid);
+  });
 
 $.ajax({
   url: 'http://localhost:8000/api/produkt/gib/' + id,
@@ -51,37 +53,19 @@ $.ajax({
 
 $('#Bild').attr('src', 'http://127.0.0.1:5500/frontend/' + response.produktbild);
 //$('#Bild').html('<img src="http://127.0.0.1:5500/frontend/' + response.produktbild + '?v=' + new Date().getTime() + '">');//
-//$('#Bild').html('<img src="http://127.0.0.1:5500/frontend/' + response.produktbild + ' ">');//
-$('#produktId').text(response.id); 
-$('#verfuegbarkeit').text(response.verfuegbarkeit);
-$('#produktBezeichnung').text(response.bezeichnung);
-$('#bruttopreis').text(formatToEuro(response.bruttopreis));
-$('#produktBeschreibung').text(response.beschreibung);
-
-// Assuming response.details contains the text with '\n' characters
+$('.produktId').text('ID: '+ response.id);  
+$('.verfuegbarkeit').text('Verfügbarkeit: ' + response.verfuegbarkeit);
+$('.produktBezeichnung').text(response.bezeichnung);
+$('.bruttopreis').text(formatToEuro(response.bruttopreis));
+$('.produktBeschreibung').text(response.beschreibung);
 var formattedDetails = response.details.replace(/\/n/g, '<br>');
-$('#details').text(response.details);
-// Update the HTML element with the formatted details
 $('#details').html(formattedDetails);
-
-
-
-
-/*
-}
-
- if (response.bilder != null) {
-  if (response.bilder.length > 0) {
-      var pic = response.bilder[0];
-      $('#productPicture').html('<img src="http://localhost:8000/' + pic.bildpfad + '">');
-  }
-} */
-/* src="http://127.0.0.1:5500/frontend/Bilder/Multivitamin.png" */
 
 }).fail(function (jqXHR, statusText, error) {
   console.log('Response Code: ' + jqXHR.status + ' - Fehlermeldung: ' + jqXHR.responseText);
   alert('Ein Fehler ist aufgetreten');
 }); 
+
 });
 
 function formatToEuro(val) {
@@ -91,12 +75,4 @@ function formatToEuro(val) {
   return asString.replace('.', ',') + " €";
 }
 
-$(document).on('click', '.addToCart', function () {     
-  // holt html Attribut: data-productid aus slider
-  var selectedProductid = $(this).data('productid');    
 
-  // Ausgabe in Browser Console
-  console.log("Produkt in den Warenkorb: " + selectedProductid);   
-  // addToBasket Funktion befindet sich in warenkorb.js 
-  addToBasket(selectedProductid);
-});
