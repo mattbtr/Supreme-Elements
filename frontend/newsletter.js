0b8b05c9b930c0b9c5af2a488c257ef689453b20
mattbtr <m.beiterhhh@googlemail.com>
2024-01-23 18:04:29 +0100

    esaf

--- frontend/newsletter.js
+++ frontend/newsletter.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    $(".newsletterForm").submit(function (event) {
+    $(document).on("click", ".subscribe", function (event) {
         console.log("form submit called");
-
-        // disable default event
-        event.preventDefault();
 
         // convert data of form to object
         let formData = {
@@ -17,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
             type: "POST",
             data: JSON.stringify(formData),
             contentType: "application/json",
-            dataType: "text",   // muss auf text sein, da server hier mit text antwortet und nicht mit einem json-objekt. Aber warum?? Vllt weil server nur ein einzelenes attribut quasi im response zurückschickt und dann das json objekt als string gilt???
+            //dataType: "text",   // muss auf text sein, da server hier mit text antwortet und nicht mit einem json-objekt. Aber warum?? Vllt weil server nur ein einzelenes attribut quasi im response zurückschickt und dann das json objekt als string gilt???
         }).done(function (response) {
             console.log(response);
             alert("Sie sind ab sofort bei unserem Newsletter angemeldet.");
@@ -28,8 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 console.log('Response Code:', jqXHR.status);
                 console.log('Fehlermeldung:', jqXHR.responseText);
                 alert("Sie haben keine gültige E-Mail-Adresse eingegeben.");
-                $(".email-newsletter").val("");
+                
             });
+        $(".email-newsletter").val("");
     });
 
 });
